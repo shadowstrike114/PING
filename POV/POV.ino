b377087a17588707047bcc8f4b7363d5036a3386
shadowstrike114 <maximilian-junk@outlook.com>
2014-07-21 21:46:24 +0200

    Datum und Kommentare

--- POV/POV.ino
+++ POV/POV.ino
@@ -1,6 +1,5 @@
-#include <TinyGPS++.h>
+#include <TinyGPS++.h>//nicht benötigte Funktionen aus der Library entfernt, um SRAM zu sparen
 #include <DHT.h>
-#include <SPI.h>
 #include "ascii.h"
 
 //Temperatur- und Luftfeuchtigkeitssensor
@@ -13,6 +12,7 @@ TinyGPSPlus gps;
 //FORMAT: GRB
 #define ARRAYLEN 43
 uint8_t buffer[ARRAYLEN][8][3];
+
 volatile uint32_t time;
 
 uint8_t mode = 0;
@@ -21,13 +21,17 @@ uint8_t modeprev = 0;
 uint8_t hours = 0;
 uint8_t minutes = 0;
 volatile uint8_t seconds = 0;
+
+boolean minutePassed = true;
+
 uint8_t hum = 0;
 uint8_t temp = 0;
-boolean minutePassed = true;
 
 
 void setup() {
+  //Serial zur Kommunikation mit GPS Modul
   Serial.begin(9600);
+  
   //Timer 1 als uhrzeitgeber via interrupt
   TCCR1A = 0;
   TCCR1B = 0;
@@ -37,11 +41,12 @@ void setup() {
   TCCR1B |= (1 << WGM12); // CTC modus
   TCCR1B |= (1 << CS12) | (1 << CS10); // Prescaler  auf 1024
   TIMSK1 |= (1 << OCIE1A); //compare interrupt an
-
+  
+  //Kompletten PORTB auf OUTPUT schalten
   DDRB = 0xFF;
 
   //GPS Daten empfangen
-  while (!gps.time.isUpdated() && millis() < 5000) {
+  while (!gps.time.isUpdated() || !gps.date.isUpdated() && millis() < 10000) {
     while (Serial.available() > 0)
       gps.encode(Serial.read());
   }
@@ -52,8 +57,9 @@ void setup() {
   }
 
   pinMode(2, INPUT_PULLUP);
+  
   attachInterrupt(0, hallInterrupt, FALLING);
-
+  
   dht.begin();
 }
 
@@ -73,10 +79,12 @@ ISR(TIMER1_COMPA_vect) {
 }
 
 void loop() {
-  if (seconds > 20 && seconds <= 50) {
+  
+  //Automatischer Moduswechsel
+  if (seconds > 20 && seconds <= 40) {
     mode = 1;
   }
-  else if (seconds > 50) {
+  else if (seconds > 40) {
     mode = 2;
   }
   else {
@@ -88,9 +96,10 @@ void loop() {
     modeprev = mode;
   }
 
-  //1 mal pro minute Temperatur und Luftfeuchtigkeit updaten
-  if (minutePassed)
-  {
+  
+  
+  //1 mal pro Minute Temperatur und Luftfeuchtigkeit updaten
+  if (minutePassed){
     hum = dht.readHumidity();
     temp = dht.readTemperature();
     minutePassed = false;
@@ -106,15 +115,41 @@ void loop() {
     sprintf(bufferH, "%02d", hours);
     sprintf(bufferM, "%02d", minutes);
     sprintf(bufferS, "%02d", seconds);
+    
     strToAry(bufferH, 0, 255, 0, 255);
+    
     strToAry(":", 11, 128, 0, 255);
+    
     strToAry(bufferM, 15, 255, 0, 255);
+    
     strToAry(":", 26, 128, 0, 255);
+    
     strToAry(bufferS, 30, 255, 0, 255);
   }
+  
+  // Datum
+  if(mode == 1){
+      char bufferY[3];
+      char bufferM[3];
+      char bufferD[3];
+      
+      sprintf(bufferY,"%02d",gps.date.year() - 2000);
+      sprintf(bufferM,"%02d",gps.date.month());
+      sprintf(bufferD,"%02d",gps.date.day());
+      
+      strToAry(bufferD, 0, 255, 128, 0);
+      
+      byteToRGB(0b01000000, 12, 255, 255, 0);
+      
+      strToAry(bufferM, 15, 255, 128, 0);
+      
+      byteToRGB(0b01000000, 26, 255,255, 0);
+      
+      strToAry(bufferY, 30, 255, 128, 0);
+    }
 
-  // Thermometer und Hygrometer
-  if (mode == 1) {
+  // Thermo- und Hygrometer
+  if (mode == 2) {
     char bufferT[3];
     char bufferH[3];
 
@@ -122,16 +157,21 @@ void loop() {
     sprintf(bufferT, "%02d", temp);
 
     strToAry(bufferT, 0, 0, 0, 255);
+    
+    //Gradzeichen
     byteToRGB(0b00000010, 12, 255, 255, 255);
     byteToRGB(0b00000101, 13, 255, 255, 255);
     byteToRGB(0b00000010, 14, 255, 255, 255);
+    
     strToAry("C", 15, 255, 255, 255);
+    
     strToAry(bufferH, 25, 0, 0, 255);
+    
     strToAry("%", 38, 255, 255, 255);
   }
 
   //Demo der Textfunktionen
-  if (mode == 2) {
+  if (mode == 3) {
     monoToRGB('P', 0, 255, 0, 0);
     monoToRGB('I', 5, 0, 255, 0);
     monoToRGB('N', 10, 0, 0, 255);
@@ -146,46 +186,43 @@ void loop() {
   }
 }
 
-void printTheBuffer() {
+void printTheBuffer() { //Ausgabe des Display buffer Arrays auf die LEDs
   for (int i = 0; i < ARRAYLEN; i++) {
     showBuffer(i);
     delayMicroseconds(time / 128);
   }
 }
 
-void clearBuffer() {
+void clearBuffer() {//Displaybuffer leeren
   uint8_t start = 0;
   for (int i = 0; i < ARRAYLEN; i++) {
     byteToRGB(0, start++, 0, 0, 0);
   }
 }
 
-void strToAry(char *s, int start, int r, int g, int b) {
+void strToAry(char *s, int start, int r, int g, int b) {//string nach position start in den Displaybuffer einfügen
   while ( *s != '\0') {
     monoToRGB(*s, start, r, g, b);
     *s++;
-    //eine Spalte Abstand zwischen den Buchstaben
-    //die spalte extra als leer einfügen, damit beim überschreiben keine artefakte auftreten
-    //byteToRGB(0,start+5,r,g,b);
     start += 6;
   }
 
 }
 
-void hallInterrupt() {
+void hallInterrupt() {//Interrupt durch Hallsensor, Messung der Umlaufzeit und einleiten der Ausgabe
   static uint32_t alt;
   time = micros() - alt;
   alt = micros();
   printTheBuffer();
 }
 
-void monoToRGB(int letter, int start, int r , int g, int b) {
+void monoToRGB(int letter, int start, int r , int g, int b) {//buchstaben aus dem Ascii array in Farbe in den Displaybuffer schreiben
     for (int i = start; i < start + 5; i++) {
       byteToRGB(ascii[letter - 32 ][i - start],i,r,g,b);
     }
 }
 
-void byteToRGB(uint8_t val, int start, int r , int g, int b) {
+void byteToRGB(uint8_t val, int start, int r , int g, int b) {//einzelnes byte in den Displaybuffer schreiben
   if (start < ARRAYLEN - 1) {
     for (int j = 0; j < 8; j++) {
       if (val & 1) {
@@ -204,7 +241,7 @@ void byteToRGB(uint8_t val, int start, int r , int g, int b) {
 }
 
 
-void showBuffer(int spalte) {
+void showBuffer(int spalte) {//eine Spalte des Buffers an die LEDs weitergeben
   for (int i = 7; i >= 0; i--) {
     for (int j = 0; j < 3; j++) {
       send12Bit(buffer[spalte][i][j]);
@@ -214,19 +251,29 @@ void showBuffer(int spalte) {
 }
 
 
+
+/*
+Zum Ansteuern der LEDs wird ein TLC5947 (http://www.ti.com/lit/ds/symlink/tlc5947.pdf ) verwendet.
+Pro LED müssen bei diesem 12 bit(4096 stufen) an helligkeitsdaten seriell übertragen werden. Da 
+die hardwareseitig eingebaute SPI- schnittstelle des arduino nur jeweils 8 bit ausgeben kann werden
+die daten hier "manuell" übertragen. Da digitalWrite() viel zu langsam ist werden die Ports direkt angesprochen.
+(erreichte geschwindigkeit ~= 70% der von SPI, es lohnt sich also nicht aufwändige bitmanipulation zu betreiben,
+um das schnellere spi verwenden zu können)
+*/
 void send12Bit(uint16_t data)
 {
   for (int i = 0; i < 12; i++)
   {
     if (data & 2048)
-    {
-      //      clock:9|10:Signal
+    {//          PINS
+      //    Signal:10|9:Clock
       PORTB = 0b00000100;
       PORTB = 0b00000110;
       PORTB = 0b00000000;
       //Signal wird bei rising Edge der CLK eingelesen, deswegen ist gleichzeitig aus ok
     }
     else {
+      
       PORTB = 0b00000000;
       PORTB = 0b00000010;
       //oben werden vom compiler 2 Takte Verschenkt, deswegen hier 2 "unnötige" damit das Timing konstant bleibt
@@ -241,7 +288,7 @@ void send12Bit(uint16_t data)
 
 }
 
-void latch() //auf Pin 8
+void latch() //Pin8 nach dem übertragen der 288 bit kurz umschalten, damit die Daten vom tlc5947 ausgegeben werden
 {
   PORTB = 0b00000001;
   PORTB = 0b00001001;
